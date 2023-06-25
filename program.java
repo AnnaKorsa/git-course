363a987210c52fb2840efaad0407fd01b57640c4
Anna Kors <ankors320@gmail.com>
2023-06-25 16:57:39 +0300

    first comit

---
+++ program.java
@@ -0,0 +1,170 @@
+// import java.util.Scanner;
+// public class program {
+// 	public static void main(String[] args) {
+// 		Scanner iScanner = new Scanner(System.in);
+// 		System.out.printf("name: ");
+// 		String name = iScanner.nextLine();
+// 		System.out.printf("Привет, %s!", name);
+// 		iScanner.close();
+// 	}
+// }
+// import java.util.Scanner;
+// 	public class program {
+// 		public static void main(String[] args) {
+// 		Scanner iScanner = new Scanner(System.in);
+// 		System.out.printf("int a: ");
+// 		int x = iScanner.nextInt();
+// 		System.out.printf("double a: ");
+// 		double y = iScanner.nextDouble();
+// 		System.out.printf("%d + %f = %f", x, y, x + y);
+// 		iScanner.close();
+// 	}
+// }
+
+// import java.util.Scanner;
+// 	public class program {
+// 		public static void main(String[] args) {
+// 		Scanner iScanner = new Scanner(System.in);
+// 		System.out.printf("int a: ");
+// 		boolean flag = iScanner.hasNextInt();
+// 		System.out.println(flag); 
+// 		int i = iScanner.nextInt();
+// 		System.out.println(i); 
+// 		iScanner.close();
+//  	} 
+// }
+
+// public class program {
+//  	static void sayHi() {
+//  		System.out.println("hi!");
+// }
+//  	static int sum(int a, int b) {
+//  		return a+b;
+//  } 
+//  	static double factor(int n) {
+//  		if(n==1)return 1;
+//  		return n * factor(n-1);
+//  }
+//  	public static void main(String[] args) {
+//  		sayHi(); // hi!
+//  		System.out.println(sum(1, 3)); // 4
+//  		System.out.println(factor(5)); // 120.0
+//  }}
+
+// В консоли запросить имя пользователя. 
+// В зависимости от текущего времени, 
+// вывести приветствие вида
+////"Доброе утро, <Имя>!", если время от 05:00 до 11:59
+////"Добрый день, <Имя>!", если время от 12:00 до 17:59;
+////"Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
+////"Доброй ночи, <Имя>!", если время от 23:00 до 4:59
+
+// import java.time.LocalTime;
+// import java.util.Scanner;
+
+// public class program {
+// 	public static void main(String[] args) {
+	
+// 	}
+// 	static void task0(){
+//          System.out.println("Как тебя зовут?");
+//          Scanner scanner = new Scanner(System.in);
+//          String name = scanner.nextLine();
+//          LocalTime time = LocalTime.now();
+//          int hour = time.getHour();
+//          if(hour>=5 && hour<12){
+//              System.out.println("Доброе утро " + name);
+//          } else if(hour<18 && hour>=12){
+//              System.out.println("Добрый день " + name);
+//          }  else if (hour < 23 && hour>=18){
+//              System.out.println("Добрый вечер " + name);
+//          } else {
+//              System.out.println("Доброй ночи " + name);
+//          }
+//         //  scanner.close();
+//     }
+// }
+
+
+// Дан массив двоичных чисел, например [1,1,0,1,1,1], 
+// вывести максимальное количество подряд идущих 1.
+// public class program {
+// 	public static void main(String[] args) {
+	
+// 	}
+// 	static void task1(){
+//         /*Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.*/
+//         int[] arr = {1,1,0,1,1,1,0,1,1,1,1};
+//         int maxOnes = 0;
+//         int temp = 0;
+//         for (int i = 0; i < arr.length; i++) {
+//             if(arr[i] == 1){
+//                 temp++;
+//             } else {
+//                 if (temp > maxOnes){
+//                 maxOnes = temp;}
+//                 temp = 0;
+//             }
+//         }
+//         if(temp > maxOnes){
+//             maxOnes = temp;
+//         }
+//         System.out.println(maxOnes);
+//     }
+
+
+
+
+// Во фразе "Добро пожаловать на курс по Java" 
+// переставить слова в обратном порядке.
+// static String task2(String str){
+// 	/*Во фразе "Добро пожаловать на курс по Java" переставить слова в обратном порядке.*/
+// 	String[] strArr = str.split(" ");
+// 	StringBuilder stringBuilder = new StringBuilder();
+// 	for (int i = 0; i < strArr.length; i++) {
+// 		stringBuilder.append(strArr[strArr.length-i-1]).append(" ");
+// 	}
+// 	return stringBuilder.toString();
+
+// }
+
+
+// Реализовать функцию возведения числа а в степень b. a, b из Z. Сводя количество выполняемых действий к минимуму.
+
+// Пример 1: а = 3, b = 2, ответ: 9
+
+// Пример 2: а = 2, b = -2, ответ: 0.25
+
+// Пример 3: а = 3, b = 0, ответ: 1
+
+
+// Дан массив nums = [3,2,5,3] и число val = 3.
+
+// Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
+
+// Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного, а остальные - равны ему.
+
+// public class program {
+//     public static void main(String[] args) {
+//         System.out.println(task1());
+//     }
+//     static int task1(){
+         /*Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.*/
+        // int[] arr = {1,1,0,1,1,1,0,1,1,1,1};
+        // int maxOnes = 0;
+        // int temp = 0;
+        // for (int j : arr) {
+        //     if (j == 1) {
+    //             temp++;
+    //         } else {
+    //             if (temp > maxOnes) {
+    //                 maxOnes = temp;
+    //             }
+    //             temp = 0;
+    //         }
+    //     }
+    //     if(temp > maxOnes){
+    //          maxOnes = temp;
+    //     }
+    //     return maxOnes;
+    // }}
